f58814ac6fbe4de6a153bfbb94b13ad4ba2cd12f
Augusto Solfa <augusto@solfa.net>
2022-11-02 18:49:07 -0300

    Add ranking page

---
+++ src/view/ranking/index.js
@@ -0,0 +1,35 @@
+import { Ranking } from "../../model/ranking.js";
+
+window.addEventListener("load", () => {
+  showRanking();
+});
+
+function showRanking() {
+  const root = document.querySelector("main");
+  const ranking = new Ranking().bestTimes;
+
+  for (let level in ranking) {
+    console.log(level);
+    const levelDiv = document.createElement("div");
+    levelDiv.classList.add("level");
+    root.appendChild(levelDiv);
+    const header = document.createElement("h2");
+    header.innerText = level;
+    levelDiv.appendChild(header);
+    for (let row in ranking[level]) {
+      const rowData = ranking[level][row];
+      const date = new Date(parseInt(rowData.date)).toDateString();
+      const rowNode = document.createElement("p");
+      rowNode.innerHTML = `<span>${date}</span><span>${formatTime(
+        rowData.seconds
+      )}</span>`;
+      levelDiv.appendChild(rowNode);
+    }
+  }
+}
+
+function formatTime(seconds) {
+  const minutes = ("0" + Math.floor(seconds / 60)).slice(-2, 6);
+  const secondsRemain = ("0" + (seconds % 60)).slice(-2, 6);
+  return `${minutes}:${secondsRemain}`;
+}
